11ef87ac2f6a71d53536b305482b8b9d427da0ab
Nelwhix <nelsonisioma1@gmail.com>
2022-08-22 20:49:41 +0100

    tried to change text editors

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -1,9 +1,13 @@
 <script>
 import { QuillEditor } from '@vueup/vue-quill'
-import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
 
 export default {
   name: "CreatePost",
+  data() {
+    return {
+    }
+  },
   components: {
     QuillEditor
   }
@@ -12,7 +16,20 @@ export default {
 
 <template>
   <section class="my-5">
-    <QuillEditor theme="snow" />
+    <div>
+      <input type="text" placeholder="Enter Blog Title" v-model="blogTitle">
+      <div>
+        <label for="blogPhoto">Upload Cover Photo</label>
+        <input type="file" ref="blogPhoto" accept=".png, .jpg, .jpeg" />
+        <button>Preview Photo</button>
+        <span>File chosen: </span>
+      </div>
+      <QuillEditor theme="snow" toolbar="full"/>
+    </div>
+    <div>
+      <button>Publish Post</button>
+      <router-link to="#" class="link">Post Preview</router-link>
+    </div>
   </section>
 </template>
 
